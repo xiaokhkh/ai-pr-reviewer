1900c6d610e5a8ed8676c090d41172f6a59ebed0
xiaokhkh <727070793@qq.com>
2025-12-31 15:26:50 +0800

    feat: 移除 token 限制，设置为无限制

--- src/limits.ts
+++ src/limits.ts
@@ -6,21 +6,10 @@ export class TokenLimits {
 
   constructor(model = 'gpt-3.5-turbo') {
     this.knowledgeCutOff = '2021-09-01'
-    if (model === 'gpt-4-32k') {
-      this.maxTokens = 32600
-      this.responseTokens = 4000
-    } else if (model === 'gpt-3.5-turbo-16k') {
-      this.maxTokens = 16300
-      this.responseTokens = 3000
-    } else if (model === 'gpt-4') {
-      this.maxTokens = 8000
-      this.responseTokens = 2000
-    } else {
-      this.maxTokens = 4000
-      this.responseTokens = 1000
-    }
-    // provide some margin for the request tokens
-    this.requestTokens = this.maxTokens - this.responseTokens - 100
+    // Set unlimited tokens (using a very large number)
+    this.maxTokens = Number.MAX_SAFE_INTEGER
+    this.responseTokens = Number.MAX_SAFE_INTEGER
+    this.requestTokens = Number.MAX_SAFE_INTEGER
   }
 
   string(): string {
